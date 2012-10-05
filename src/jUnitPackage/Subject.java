530a3f08aefde86534079b237dd082cba0968ab7
marwi509 <marwi509@student.liu.se>
2012-10-05 11:46:29 +0200

    Fixed it, and fixed the bug in the "multiply" function.

--- src/jUnitPackage/Subject.java
+++ src/jUnitPackage/Subject.java
@@ -2,13 +2,13 @@ package jUnitPackage;
 
 import java.util.Vector;
 
-public class Subject 
+public class Subject {
 	public int add(int n1, int n2){
 		return n1 + n2;
 	}
 	
 	public int multiply(int n1, int n2){
-		return n1/n2;
+		return n1*n2;
 	}
 	
 	public void crash(){
